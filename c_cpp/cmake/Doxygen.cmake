10254c38e6e35724f2361f2dc29aa17a0c280283
Christof Pieloth <cpieloth@gmx.net>
2018-03-19 22:47:11 +0100

    [ADD] Add markdown documentation to doxygen.

--- c_cpp/cmake/Doxygen.cmake
+++ c_cpp/cmake/Doxygen.cmake
@@ -4,6 +4,7 @@
 
 ##
 # Add a target with related command to call doxygen for given config file.
+# @note: Output directory depends on config file and not CMAKE_BINARY_DIR!
 #
 # @param target_name  Target name to use for the command.
 # @param config_name  Doxygen config file to use.
@@ -14,7 +15,11 @@ function(doxygen_create target_name config_name)
         message(WARNING "Doxygen documentation is not available. Please install doxygen.")
         return()
     endif()
-    
+
+    # get and use directory of config file as working directory,
+    # such that relative paths belongs to config file location
+    get_filename_component(working_dir ${config_name} DIRECTORY)
     add_custom_target(${target_name} ${DOXYGEN} ${config_name}
+                      WORKING_DIRECTORY ${working_dir}
                       COMMENT "Call doxygen for config file: ${config_name}")
 endfunction(doxygen_create)
---
+++ c_cpp/doc/manual/getting_started.md
@@ -0,0 +1,23 @@
+# Getting Started
+
+## Compile Example
+
+    $ cd project_templates/c_cpp
+    $ mkdir build
+    $ cmake ..
+    $ make
+
+
+## Generate Documentation
+
+    $ cd project_templates/c_cpp
+    $ mkdir build
+    $ cmake ..
+    $ make documentation
+
+
+## Run Example
+
+    $ cd project_templates/c_cpp/build
+    $ src/example/example -h
+    $ src/example/example -f foo -l bar
