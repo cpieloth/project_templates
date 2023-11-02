a00e1df9f72204cdd5ced21775b9bc3266c8ee55
Christof Pieloth <cpieloth@gmx.net>
2023-11-02 22:26:16 +0100

    [ADD] Linting with clang-tidy.

---
+++ cpp_conan/cmake/ClangTidy.cmake
@@ -0,0 +1,13 @@
+option(SKIP_CLANG_TIDY "Enable/disable linting with clang-tidy." ON)
+
+if(${SKIP_CLANG_TIDY})
+    message(STATUS "Skip linting: clang-tidy")
+    return()
+endif()
+
+find_program(CLANG_TIDY_EXEC NAMES clang-tidy clang-tidy-17 REQUIRED)
+
+message(STATUS "Found clang-tidy: ${CLANG_TIDY_EXEC}")
+
+set(CMAKE_C_CLANG_TIDY ${CLANG_TIDY_EXEC})
+set(CMAKE_CXX_CLANG_TIDY ${CLANG_TIDY_EXEC})
